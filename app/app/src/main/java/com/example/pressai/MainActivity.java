67086661b107bbaae7b5cb27519ec747cb6ebf77
Yousran <yusranmazidan@gmail.com>
2024-03-23 17:26:02 +0800

    FEAT: Signup layout

--- app/app/src/main/java/com/example/pressai/MainActivity.java
+++ app/app/src/main/java/com/example/pressai/MainActivity.java
@@ -5,20 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ClickableSpan;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button login_btn;
+    Button login_btn,link_signup_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         login_btn = findViewById(R.id.login_btn);
+        link_signup_btn = findViewById(R.id.link_signup_btn);
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,5 +30,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(layout_dashboard);
             }
         });
+        link_signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent layout_signup = new Intent(getApplicationContext(), Signup.class);
+                startActivity(layout_signup);
+            }
+        });
     }
+
 }
