ae313c83d3ceb180599f2cd7de2e34e2a76e4db1
Yousran <yusranmazidan@gmail.com>
2024-03-23 15:58:14 +0800

    FEAT: Login and Dashboard Layout

---
+++ app/app/src/main/java/com/example/pressai/Dashboard.java
@@ -0,0 +1,40 @@
+package com.example.pressai;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.navigation.NavigationBarView;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Dashboard extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        NavigationBarView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.item_1) {
+                    // Handle item 1 reselection
+                    return true;
+                } else if (id == R.id.item_2) {
+                    // Handle item 2 reselection
+                    return true;
+                } else if (id == R.id.item_3) {
+                    // Handle item 3 reselection
+                    return true;
+                } else if (id == R.id.item_4) {
+                    // Handle item 4 reselection
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+    }
+}
--- app/app/src/main/java/com/example/pressai/MainActivity.java
+++ app/app/src/main/java/com/example/pressai/MainActivity.java
@@ -3,38 +3,28 @@ package com.example.pressai;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NavigationBarView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        login_btn = findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                if (id == R.id.item_1) {
-                    // Handle item 1 reselection
-                    return true;
-                } else if (id == R.id.item_2) {
-                    // Handle item 2 reselection
-                    return true;
-                } else if (id == R.id.item_3) {
-                    // Handle item 3 reselection
-                    return true;
-                } else if (id == R.id.item_4) {
-                    // Handle item 4 reselection
-                    return true;
-                }
-                return false;
+            public void onClick(View v) {
+                Intent layout_dashboard = new Intent(getApplicationContext(), Dashboard.class);
+                startActivity(layout_dashboard);
             }
         });
-
     }
 }
