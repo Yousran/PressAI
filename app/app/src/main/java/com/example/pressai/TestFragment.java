559669b3bf94d68d460b7ae0562694be1a8fac79
Yousran <yusranmazidan@gmail.com>
2024-05-10 08:35:38 +0800

    FIX: Riwayat Test

--- app/app/src/main/java/com/example/pressai/TestFragment.java
+++ app/app/src/main/java/com/example/pressai/TestFragment.java
@@ -1,6 +1,9 @@
 package com.example.pressai;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,10 +12,24 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class TestFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
@@ -21,6 +38,12 @@ public class TestFragment extends Fragment {
 
     private String mParam1;
     private String mParam2;
+
+    private RecyclerView recyclerViewUjian;
+    DatabaseReference databaseTest;
+    RiwayatUjianAdapter riwayatUjianAdapter;
+    ArrayList<DataUjian> dataUjian;
+    String username;
 
     public TestFragment() {
         // Required empty public constructor
@@ -47,8 +70,14 @@ public class TestFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        View view = inflater.inflate(R.layout.fragment_test, container, false);
+
+        SharedPreferences sharedPref = getActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        username = sharedPref.getString("username", "Mahasiswa");
+
+        setupRecyclerViewUjian(view);
+
+        return view;
     }
 
     @Override
@@ -60,12 +89,6 @@ public class TestFragment extends Fragment {
         join_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                TestLobbyFragment testLobbyFragment = new TestLobbyFragment();
-//                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentTransaction.replace(R.id.fragment_test, testLobbyFragment);
-//                fragmentTransaction.addToBackStack(null);
-//                fragmentTransaction.commit();
                 ScanTestFragment scanTestFragment = new ScanTestFragment();
                 FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -74,6 +97,42 @@ public class TestFragment extends Fragment {
                 fragmentTransaction.commit();
             }
         });
+
+        Query queryujian = databaseTest.orderByChild("tanggal_test");
+        queryujian.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                dataUjian.clear();
+                ArrayList<DataSnapshot> dataList = new ArrayList<>();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    dataList.add(dataSnapshot);
+                }
+
+                Collections.reverse(dataList);
+                for (DataSnapshot dataSnapshot : dataList){
+                    DataUjian data = dataSnapshot.getValue(DataUjian.class);
+                    dataUjian.add(data);
+                }
+                riwayatUjianAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void setupRecyclerViewUjian(View view) {
+        recyclerViewUjian = view.findViewById(R.id.recycler_ujian);
+        databaseTest = FirebaseDatabase.getInstance().getReference("users/" + username + "/test/");
+
+        recyclerViewUjian.setHasFixedSize(true);
+        recyclerViewUjian.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        dataUjian = new ArrayList<>();
+        riwayatUjianAdapter = new RiwayatUjianAdapter(getContext(),dataUjian);
+        recyclerViewUjian.setAdapter(riwayatUjianAdapter);
     }
 
 }
